90ef20e6c5b343f86c7e2771a567d206b5a2b3bb
Justin Clark <justindanielclark@gmail.com>
2023-07-28 12:37:18 -0700

    + basic supertest test for route

---
+++ backend/tests/unit/routes/apiLogin.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+import app from "../../../services/app";
+
+describe("/api/login", () => {
+  //TODO
+  it("GET", async () => {
+    const response = await request(app).get("/api/login");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("login route get");
+  });
+  it("POST", async () => {
+    const response = await request(app).post("/api/login");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("login route post");
+  });
+});
