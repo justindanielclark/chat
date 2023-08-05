3e9dda25b9cdf986366e9af5aca3fbe12da10f0a
JClark <justindanielclark@gmail.com>
2023-08-05 15:11:40 -0700

    Added Filename and Func Loc to Error Msg.

--- backend/src/utils/errors/ProcessEnvNotConfiguredError.ts
+++ backend/src/utils/errors/ProcessEnvNotConfiguredError.ts
@@ -3,7 +3,7 @@ class ProcessEnvNotConfiguredError extends CustomError {
   public error_name: string = "ProcessEnvNotConfiguredError";
   public constructor(occurrredInFilename: string, occurredInFunction: string) {
     super(
-      "Attempt to use process.env property without property being initialized",
+      `Attempt to use process.env property without property being initialized:\nOccurred In File: ${occurrredInFilename}\nOccurred In Func: ${occurredInFunction}`,
       occurredInFunction,
       occurrredInFilename,
     );
