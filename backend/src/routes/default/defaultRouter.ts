372abacccb9508919e97206d4c3fc8bf923b0322
JClark <justindanielclark@gmail.com>
2023-09-14 18:31:02 -0700

    removed session store, rewrked static asset path

--- backend/src/routes/default/defaultRouter.ts
+++ backend/src/routes/default/defaultRouter.ts
@@ -5,7 +5,8 @@ const router = Router();
 
 router.get("*", (req: Request, res: Response) => {
   //Stupid, but it works -> need pathing based of build dist after 'compile'
-  return res.sendFile(path.join(__dirname, "..", "..", "..", "..", "_frontend_build", "index.html"));
+  const fileName = "index.html";
+  return res.sendFile(path.join(__dirname, "..", "..", "..", "..", "..", "_frontend_build", "index.html"));
 });
 
 export default router;
