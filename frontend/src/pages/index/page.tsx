3d848be184a4e0f1f388d1adace1b733a8d4e197
JClark <justindanielclark@gmail.com>
2023-09-16 09:31:34 -0700

    Index Page / Signin Page Buildout

--- frontend/src/pages/index/page.tsx
+++ frontend/src/pages/index/page.tsx
@@ -1,7 +1,26 @@
 export default function Page() {
   return (
-    <div className="flex flex-col gap-6">
-      <div>Just an Index Page!</div>
+    <div className="">
+      <form className="p-2 max-w-sm bg-red-200 mx-auto">
+        <div>
+          <h1 className="text-xl font-bold">Sign-In:</h1>
+          <div className="flex flex-col">
+            <label>Username:</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="flex flex-col">
+            <label>Password:</label>
+            <input type="text" name="" id="" />
+          </div>
+        </div>
+        <hr className="border-black m-4" />
+        <div className="w-full flex flex-col">
+          <h1 className="text-xl font-bold">Sign-Up:</h1>
+          <button className=" bg-green-400 p-2 rounded-lg mx-auto">
+            Click Here To Sign-Up!
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- frontend/src/pages/layout/RootLayout.tsx
+++ frontend/src/pages/layout/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
     <>
-      <header className="bg-red-100 p-1">HEADER</header>
+      <header className="bg-red-100 p-1 sticky top-0">HEADER</header>
       <main className="bg-blue-100 flex-1">
         <Outlet />
       </main>
